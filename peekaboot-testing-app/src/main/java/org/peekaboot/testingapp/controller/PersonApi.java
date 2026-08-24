96d8afa6bcbbf38eb20c23d9d3771b71105e3bc2
Raphael <raphael@peekaboot.org>
2026-08-24 09:51:06 +0200

    refactor: drop unused logger field from PersonApi

--- peekaboot-testing-app/src/main/java/org/peekaboot/testingapp/controller/PersonApi.java
+++ peekaboot-testing-app/src/main/java/org/peekaboot/testingapp/controller/PersonApi.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import org.peekaboot.testingapp.PersonQueryService;
 import org.peekaboot.testingapp.entity.Person;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonApi {
-
-    private static final Logger log = LoggerFactory.getLogger(PersonApi.class);
 
     private final PersonQueryService personQueryService;
 
