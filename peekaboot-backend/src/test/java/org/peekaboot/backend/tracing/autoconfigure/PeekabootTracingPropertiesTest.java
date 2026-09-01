c08f3533a93570167c88f9a6b634244e213f01f5
Raphael <raphael@peekaboot.org>
2026-09-01 14:45:57 +0200

    style(peekaboot-backend): format PeekabootTracingPropertiesTest with palantir

--- peekaboot-backend/src/test/java/org/peekaboot/backend/tracing/autoconfigure/PeekabootTracingPropertiesTest.java
+++ peekaboot-backend/src/test/java/org/peekaboot/backend/tracing/autoconfigure/PeekabootTracingPropertiesTest.java
@@ -1,8 +1,8 @@
 package org.peekaboot.backend.tracing.autoconfigure;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 class PeekabootTracingPropertiesTest {
 
