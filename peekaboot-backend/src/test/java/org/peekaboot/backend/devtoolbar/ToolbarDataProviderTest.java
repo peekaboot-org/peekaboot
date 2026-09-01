4607678fa57bc73e8fd102b6e170197803837249
Raphael <raphael@peekaboot.org>
2026-09-01 15:25:43 +0200

    style(backend): format ToolbarDataProviderTest with palantir

--- peekaboot-backend/src/test/java/org/peekaboot/backend/devtoolbar/ToolbarDataProviderTest.java
+++ peekaboot-backend/src/test/java/org/peekaboot/backend/devtoolbar/ToolbarDataProviderTest.java
@@ -1,9 +1,9 @@
 package org.peekaboot.backend.devtoolbar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ToolbarDataProviderTest {
 
